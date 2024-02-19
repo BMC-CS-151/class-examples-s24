49095a412238b776abefea1fe6e77d50497adf3f
Elizabeth Dinella <elizabeth.a.dinella@gmail.com>
2024-02-19 14:31:12 -0500

    lec 09 code

---
+++ lec09/ADT.java
@@ -0,0 +1,13 @@
+import java.util.Queue;
+import java.util.ArrayDeque;
+
+public class ADT {
+    private Queue<Integer> q;
+    private Student s;
+
+    public ADT() {
+       s = new Student();
+       q = new ArrayDeque<Integer>(); 
+    }
+
+}
